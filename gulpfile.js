d60caf8795c6ccf4757185f72c21b61b8fe8e78e
Fan Zhang <bl4ck5unxx@gmail.com>
2016-01-08 11:36:16 -0500

    update

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp  = require('gulp'),
+	watch = require('./semantic/tasks/watch'), 
+	build = require('./semantic/tasks/build');
+
+// import task with a custom task name
+gulp.task('watch', watch);
+gulp.task('build', build);
+
+gulp.task('default', function (){
+	gulp.start('watch');
+});
