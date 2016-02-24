555cdc5ae37e9e606357a44c5775a6c09be6d7db
Fan Zhang <bl4ck5unxx@gmail.com>
2016-02-24 14:45:47 -0500

    update

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,3 @@ popd
 
 jekyll b
 
-rsync -avh --delete _site/ fanz@lion.cs.cornell.edu:/home/WIN/fanz/MyWeb/
-
---
+++ push.sh
@@ -0,0 +1,3 @@
+jekyll b
+rsync -avh --delete _site/ fanz@lion.cs.cornell.edu:/home/WIN/fanz/MyWeb/
+
